fd31029a8e87ef8cbe7acc71d16bb6016bb1b5ed
Alex <alexrudall@users.noreply.github.com>
2020-03-26 11:00:50 +0000

    Rename file to delimiter separated values for accuracy

--- spec/services/etl/import_job_spec.rb
+++ spec/services/etl/import_job_spec.rb
@@ -1,7 +1,7 @@
 describe ETL::ImportJob do
   it 'creates a new PersonRecord', focus: true do
     job = ETL::ImportJob.setup(
-      filename: file_fixture('person_record.csv').to_path
+      filename: file_fixture('person_record.dsv').to_path
     )
     Kiba.run(job)
     expect(PersonRecord.count).to eq(1)
