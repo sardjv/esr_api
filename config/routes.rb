a6122241950308b67af622575094beba653fe1ad
Alex <alexrudall@gmail.com>
2020-03-26 14:36:01 +0000

    Add basic auth for sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   # Sidekiq dashboard.
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == 'admin' && password == 'todo_set_from_env'
+  end
   mount Sidekiq::Web => '/sidekiq'
 end
