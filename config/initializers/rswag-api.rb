9e3ec86e47329de46e421f5b8d0f994fc5fb8b5e
Alex <alexrudall@users.noreply.github.com>
2020-04-02 18:27:11 +0100

    Fix Rubocop errors

--- config/initializers/rswag-api.rb
+++ config/initializers/rswag-api.rb
@@ -1,5 +1,4 @@
 Rswag::Api.configure do |c|
-
   # Specify a root folder where Swagger JSON files are located
   # This is used by the Swagger middleware to serve requests for API descriptions
   # NOTE: If you're using rswag-specs to generate Swagger, you'll need to ensure
