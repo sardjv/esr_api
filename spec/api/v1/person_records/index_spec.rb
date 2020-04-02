ac471f4d8b70a0d4cf5fa2e4f1a7f1b62d0a960e
Alex <alexrudall@users.noreply.github.com>
2020-04-02 14:46:54 +0100

    Add docs for index action

--- spec/api/v1/person_records/index_spec.rb
+++ spec/api/v1/person_records/index_spec.rb
@@ -1,28 +1,28 @@
 require 'swagger_helper'
 
 describe 'Api::V1::PersonRecordResource', type: :request, swagger_doc: 'v1/swagger.json' do
-  let(:person_record) { create(:person_record) }
+  let!(:person_record) { create(:person_record) }
   let(:response_data) { JSON.parse(response.body)['data'] }
 
-  path '/api/v1/person_records/{id}' do
-    get 'person record' do
+  path '/api/v1/person_records' do
+    get 'person records' do
       tags 'PersonRecord'
       # security [{ apiToken: [] }, { apiEmail: [] }]
       produces 'application/vnd.api+json'
-      parameter name: :id, in: :path, type: :string, required: true
-
-      let(:id) { person_record.id }
+      parameter name: 'page', in: :query, type: :string, required: false
+      parameter name: 'per_page', in: :query, type: :string, required: false
 
       context 'when a normal user' do
         # let('X-API-TOKEN') { normal_user.authentication_token }
         # let('X-API-EMAIL') { normal_user.email }
 
         response '200', 'successful' do
-          schema '$ref' => '#/definitions/person_record_response'
+          schema '$ref' => '#/definitions/person_records_response'
 
           describe 'attributes match database values' do
             run_test! do
-              response_data['attributes'].each do |key, value|
+              expect(response_data.count).to eq(1)
+              response_data.first['attributes'].each do |key, value|
                 expect(person_record.send(key)).to eq(value)
               end
             end
