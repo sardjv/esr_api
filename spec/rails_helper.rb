dbf36b3305c176f60a2b09efe4bd5b73de6047e8
Alex <alexrudall@gmail.com>
2020-04-02 18:27:11 +0100

    Fix Rubocop errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,10 +41,9 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation, except: %w(ar_internal_metadata))
+    DatabaseCleaner.clean_with(:truncation, except: %w[ar_internal_metadata])
   end
 
   config.around(:each) do |example|
