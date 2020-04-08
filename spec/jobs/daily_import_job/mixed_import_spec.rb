1690bf02bb57d7900fa5f0090e4b4eeae4f31328
Alex <alexrudall@users.noreply.github.com>
2020-04-08 11:09:27 +0100

    Check timestamps get set

--- spec/jobs/daily_import_job/mixed_import_spec.rb
+++ spec/jobs/daily_import_job/mixed_import_spec.rb
@@ -7,21 +7,31 @@ describe DailyImportJob, type: :job do
       .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
   end
 
-  it 'creates new records' do
-    perform_enqueued_jobs { job }
+  context 'with records created' do
+    before { perform_enqueued_jobs { job } }
 
-    expect(AbsenceRecord.count).to eq(1)
-    expect(PersonRecord.count).to eq(1)
+    it 'sets attributes correctly' do
+      expect(AbsenceRecord.count).to eq(2)
+      expect(PersonRecord.count).to eq(2)
 
-    absence_record = AbsenceRecord.first
-    person_record = PersonRecord.first
+      absence_record = AbsenceRecord.first
+      person_record = PersonRecord.first
 
-    # Expect values in the database to match input from mixed_record.dsv.
-    ImportExpectations.absence_record.each do |key, value|
-      expect(absence_record.send(key)).to eq(value)
-    end
-    ImportExpectations.person_record.each do |key, value|
-      expect(person_record.send(key)).to eq(value)
+      # Expect values in the database to match input from mixed_record.dsv.
+      ImportExpectations.absence_record.each do |key, value|
+        expect(absence_record.send(key)).to eq(value)
+      end
+      ImportExpectations.person_record.each do |key, value|
+        expect(person_record.send(key)).to eq(value)
+      end
+
+      # Adds timestamps.
+      [AbsenceRecord, PersonRecord].each do |klass|
+        klass.all.each do |record|
+          expect(record.created_at).not_to eq(nil)
+          expect(record.updated_at).not_to eq(nil)
+        end
+      end
     end
   end
 end
