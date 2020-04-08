88c6ac8fe6eaf56fe893e18cc9081c01888ce229
Alex <alexrudall@users.noreply.github.com>
2020-04-08 16:08:35 +0100

    Remove POST and PUT API endpoints

--- app/resources/api/v1/absence_record_resource.rb
+++ app/resources/api/v1/absence_record_resource.rb
@@ -1,4 +1,6 @@
 class Api::V1::AbsenceRecordResource < JSONAPI::Resource
+  immutable # Remove POST and PUT endpoints.
+
   model_name 'AbsenceRecord'
 
   attributes *ETL::Headers::AbsenceRecord.all
