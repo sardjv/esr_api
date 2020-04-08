8c3c99e198448dbbaed173df7568af963196100d
Alex <alexrudall@users.noreply.github.com>
2020-04-08 10:42:28 +0100

    Add mixed row type import file spec, with some reorganisation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,7 @@ require 'rspec/rails'
 require 'support/factory_bot'
 require 'database_cleaner/active_record'
 require 'database_cleaner/redis'
+require 'support/expectations/import_expectations'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -54,6 +55,7 @@ RSpec.configure do |config|
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include ImportExpectations
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
