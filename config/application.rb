f73af2e173d2b11ee2bba1aff9c82f05e70cc7c3
Alex <alexrudall@users.noreply.github.com>
2020-04-09 12:35:04 +0100

    Remove references to ActionCable

--- config/application.rb
+++ config/application.rb
@@ -56,14 +56,6 @@ module EsrApi
     config.active_job.queue_name_prefix =
       "#{ENV['ACTIVE_JOB_QUEUE_PREFIX']}_#{Rails.env}"
 
-    # Action Cable setting to de-couple it from the main Rails process.
-    config.action_cable.url = ENV['ACTION_CABLE_FRONTEND_URL']
-
-    # Action Cable setting to allow connections from these domains.
-    origins = ENV['ACTION_CABLE_ALLOWED_REQUEST_ORIGINS'].split(',')
-    origins.map! { |url| /#{url}/ }
-    config.action_cable.allowed_request_origins = origins
-
     # Bypass CORS protection to allow UI on port 3000 to access API.
     config.middleware.insert_before 0, Rack::Cors do
       allowed_headers = %i[get options head]
