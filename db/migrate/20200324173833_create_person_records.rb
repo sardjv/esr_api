1e5b7ad0dd62fc2df358e71bca7a740be1ecb539
Alex <alexrudall@gmail.com>
2020-04-14 11:20:57 +0100

    Squash PersonRecord migrations for simplicity

--- db/migrate/20200324173833_create_person_records.rb
+++ db/migrate/20200324173833_create_person_records.rb
@@ -1,7 +1,42 @@
 class CreatePersonRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :person_records do |t|
-      t.string :PersonID
+      t.string 'PersonID'
+      t.string 'Record Type'
+      t.date 'Effective StartDate'
+      t.date 'Effective EndDate'
+      t.string 'Employee Number'
+      t.string 'Title'
+      t.string 'LastName'
+      t.string 'FirstName'
+      t.string 'MiddleNames'
+      t.string 'MaidenName'
+      t.string 'PreferredName'
+      t.string 'Previous LastName'
+      t.string 'Gender'
+      t.date 'Date of Birth'
+      t.string 'NationalInsurance Number'
+      t.string 'NHS UniqueID'
+      t.date 'HireDate'
+      t.date 'ActualTermination Date'
+      t.string 'Termination Reason'
+      t.string 'Employee StatusFlag'
+      t.string 'WTR Opt Out'
+      t.date 'WTR Opt Out Date'
+      t.string 'Ethnic Origin'
+      t.string 'Country of Birth'
+      t.string 'Previous Employer'
+      t.string 'Previous EmployerType'
+      t.date 'CSD 3 Months'
+      t.date 'CSD 12 Months'
+      t.string 'NHS CRS UUID'
+      t.string 'System Person Type'
+      t.string 'User Person Type'
+      t.string 'Office e-mail address'
+      t.date 'NHS StartDate'
+      t.date 'Last UpdateDate'
+      t.string 'Disability Flag'
+      t.timestamp 'Ins_Upd_TimeStamp'
 
       t.timestamps
     end
