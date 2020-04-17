51805ec7246e2d8603194de9b4b043b0ca7938b4
Alex <alexrudall@users.noreply.github.com>
2020-04-17 15:11:55 +0100

    Add definitions to core

--- app/definitions/api/v1/swagger/core.rb
+++ app/definitions/api/v1/swagger/core.rb
@@ -31,9 +31,20 @@ class Api::V1::Swagger::Core
   def self.definitions
     [
       Api::V1::Swagger::AbsenceRecord.definitions,
+      Api::V1::Swagger::AssignmentRecord.definitions,
+      Api::V1::Swagger::CompetencyRecord.definitions,
+      Api::V1::Swagger::CostingRecord.definitions,
+      Api::V1::Swagger::DisabilityRecord.definitions,
+      Api::V1::Swagger::ElementRecord.definitions,
       Api::V1::Swagger::LocationRecord.definitions,
+      Api::V1::Swagger::OrganisationRecord.definitions,
+      Api::V1::Swagger::PersonEITRecord.definitions,
       Api::V1::Swagger::PersonRecord.definitions,
-      Api::V1::Swagger::PositionRecord.definitions
+      Api::V1::Swagger::PositionEITRecord.definitions,
+      Api::V1::Swagger::PositionRecord.definitions,
+      Api::V1::Swagger::QualificationRecord.definitions,
+      Api::V1::Swagger::SITRecord.definitions,
+      Api::V1::Swagger::TrainingAbsenceRecord.definitions
     ]
   end
 end
