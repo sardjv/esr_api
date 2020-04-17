673b3e8b4e77cf7d75bbe91ac3589500efbc442f
Alex <alexrudall@users.noreply.github.com>
2020-04-17 12:48:22 +0100

    Add full mixed spec

--- spec/jobs/daily_import_job/mixed_import_spec.rb
+++ spec/jobs/daily_import_job/mixed_import_spec.rb
@@ -6,27 +6,31 @@ describe DailyImportJob, type: :job do
     before { perform_enqueued_jobs { job } }
 
     it 'sets attributes correctly' do
-      expect(AbsenceRecord.count).to eq(1)
-      expect(PersonRecord.count).to eq(1)
-      expect(PositionRecord.count).to eq(1)
-      expect(LocationRecord.count).to eq(1)
+      [
+        AbsenceRecord,
+        AssignmentRecord,
+        CompetencyRecord,
+        CostingRecord,
+        DisabilityRecord,
+        ElementRecord,
+        LocationRecord,
+        OrganisationRecord,
+        PersonEITRecord,
+        PersonRecord,
+        PositionEITRecord,
+        PositionRecord,
+        QualificationRecord,
+        SITRecord,
+        TrainingAbsenceRecord
+      ].each do |klass|
+        expect(klass.count).to eq(1)
 
-      # Expect values in the database to match input from mixed_record.dsv.
-      Expectations::AbsenceRecord.updated.each do |key, value|
-        expect(AbsenceRecord.first.send(key)).to eq(value)
-      end
-      Expectations::PersonRecord.updated.each do |key, value|
-        expect(PersonRecord.first.send(key)).to eq(value)
-      end
-      Expectations::PositionRecord.updated.each do |key, value|
-        expect(PositionRecord.first.send(key)).to eq(value)
-      end
-      Expectations::LocationRecord.updated.each do |key, value|
-        expect(LocationRecord.first.send(key)).to eq(value)
-      end
+        # Expect values in the database to match input from mixed_record.dsv.
+        "Expectations::#{klass}".constantize.updated.each do |key, value|
+          expect(klass.first.send(key)).to eq(value)
+        end
 
-      # Adds timestamps.
-      [AbsenceRecord, PersonRecord, PositionRecord].each do |klass|
+        # Expect timestamps.
         klass.all.each do |record|
           expect(record.created_at).not_to eq(nil)
           expect(record.updated_at).not_to eq(nil)
