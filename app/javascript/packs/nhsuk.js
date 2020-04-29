042195e5e69441c6fbd154a627ea6fb381f6cf94
Alex <alexrudall@gmail.com>
2020-04-29 17:19:52 +0100

    Import correct NHSUK JS, maybe

--- app/javascript/packs/nhsuk.js
+++ app/javascript/packs/nhsuk.js
@@ -1,3 +1,3 @@
-require('nhsuk-frontend/all').initAll()
+require('nhsuk-frontend/packages/nhsuk.js').initAll()
 require.context('nhsuk-frontend/packages/assets', true)
 import '../stylesheets/nhsuk.scss'
