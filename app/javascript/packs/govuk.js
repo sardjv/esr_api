dd2dac8ed7f5860778ee865fdcf048e1e58a2644
Alex <alexrudall@users.noreply.github.com>
2020-04-29 17:00:13 +0100

    Switch to NHS frontend!

--- app/javascript/packs/govuk.js
+++
@@ -1,3 +0,0 @@
-require('govuk-frontend/govuk/all').initAll()
-require.context('govuk-frontend/govuk/assets/images', true)
-import '../stylesheets/govuk.scss'
---
+++ app/javascript/packs/nhsuk.js
@@ -0,0 +1,3 @@
+require('nhsuk-frontend/all').initAll()
+require.context('nhsuk-frontend/packages/assets', true)
+import '../stylesheets/nhsuk.scss'
