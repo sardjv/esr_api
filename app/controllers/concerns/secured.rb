56e740e15c6be8a9b9b5fb82ae4d51aa09d76f06
Alex <alexrudall@users.noreply.github.com>
2020-06-03 11:30:29 +0100

    Add auth override for specs

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -4,10 +4,14 @@ module Secured
   extend ActiveSupport::Concern
 
   included do
-    before_action :logged_in_using_omniauth?
+    before_action :redirect_unless_logged_in
   end
 
-  def logged_in_using_omniauth?
-    redirect_to '/' unless session[:userinfo].present?
+  def redirect_unless_logged_in
+    redirect_to '/' unless admin_authenticated?
+  end
+
+  def admin_authenticated?
+    session[:userinfo].present?
   end
 end
