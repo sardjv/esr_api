fdfc05a8ad59c7c166a03fb07400e19ff655631e
Alex <alexrudall@gmail.com>
2020-06-03 11:47:17 +0100

    Add SecuredWithToken class

---
+++ lib/tasks/json_web_token.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require 'net/http'
+require 'uri'
+
+class JsonWebToken
+  def self.verify(token)
+    JWT.decode(token, nil,
+               true, # Verify the signature of this token
+               algorithm: 'RS256',
+               iss: 'https://YOUR_DOMAIN/',
+               verify_iss: true,
+               aud: Rails.application.secrets.auth0_api_audience,
+               verify_aud: true) do |header|
+      jwks_hash[header['kid']]
+    end
+  end
+
+  def self.jwks_hash
+    jwks_raw = Net::HTTP.get URI("https://YOUR_DOMAIN/.well-known/jwks.json")
+    jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
+    Hash[
+      jwks_keys
+      .map do |k|
+        [
+          k['kid'],
+          OpenSSL::X509::Certificate.new(
+            Base64.decode64(k['x5c'].first)
+          ).public_key
+        ]
+      end
+    ]
+  end
+end
