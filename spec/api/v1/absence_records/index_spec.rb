aefc964a5d2e92102fb6b0a78fc8c4b40acc4bfc
Alex <alexrudall@users.noreply.github.com>
2020-08-05 16:17:15 +0100

    Add Authentication test

--- spec/api/v1/absence_records/index_spec.rb
+++ spec/api/v1/absence_records/index_spec.rb
@@ -16,7 +16,34 @@ describe 'Api::V1::AbsenceRecordResource', type: :request, swagger_doc: 'v1/swag
       let!(:'page[size]') { 2 }
       let!(:'page[number]') { 1 }
 
-      context 'when a normal user' do
+      context 'when not authenticated' do
+        let(:Authorization) { nil }
+
+        before do
+          module SecuredWithToken
+            def authenticate_request!
+              render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+            end
+          end
+        end
+
+        response '401', 'Error: Unauthorized' do
+          schema '$ref' => '#/definitions/error_401'
+
+          run_test!
+        end
+
+        after do
+          # Reset Auth after test.
+          module SecuredWithToken
+            def authenticate_request!
+              true
+            end
+          end
+        end
+      end
+
+      context 'when an admin' do
         let(:Authorization) { 'Bearer dummy_json_web_token' }
 
         response '200', 'successful' do
