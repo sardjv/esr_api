d013e0aa550a82db73e9136ca6a6f6ce98f718aa
Alex <alexrudall@users.noreply.github.com>
2020-09-04 11:59:37 +0100

    Redirect to admin path after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
+  def after_sign_in_path_for(resource)
+    admin_absence_records_path
+  end
 end
