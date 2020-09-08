c918663cc9e2910d9789302e78aa58409d9d17a1
Alex <alexrudall@users.noreply.github.com>
2020-09-08 15:17:27 +0100

    Link data routes to resources

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -1,11 +1,11 @@
 module DataHelper
   def self.grouped_routes
-    routes.group_by { |n| n[0].upcase }
+    routes.group_by { |n| n[:name][0].upcase }
   end
 
   def self.routes
     routes = Rails.application.routes.set.anchored_routes.map(&:defaults)
-    controllers = routes.map { |r| r[:controller].starts_with?('ui/') ? r[:controller] : nil }
-    controllers.compact.uniq.map { |c| c.split('ui/').last }
+    routes = routes.map { |r| r[:controller].starts_with?('ui/') ? r[:controller] : nil }.compact.uniq
+    routes.map { |r| { name: r.split('ui/').last.titleize, path: r } }
   end
 end
--- spec/helpers/data_helper_spec.rb
+++ spec/helpers/data_helper_spec.rb
@@ -5,16 +5,41 @@ describe DataHelper do
     it 'includes all routes' do
       expect(DataHelper.grouped_routes).to eq(
         {
-          'A' => %w[absence_records assignment_records],
-          'C' => %w[competency_records costing_records],
-          'D' => %w[disability_records],
-          'E' => %w[element_records],
-          'L' => %w[location_records],
-          'O' => %w[organisation_records],
-          'P' => %w[person_eit_records person_records position_eit_records position_records],
-          'Q' => %w[qualification_records],
-          'S' => %w[sit_records],
-          'T' => %w[training_absence_records]
+          'A' => [
+            { name: 'Absence Records', path: 'ui/absence_records' },
+            { name: 'Assignment Records', path: 'ui/assignment_records' }
+          ],
+          'C' => [
+            { name: 'Competency Records', path: 'ui/competency_records'},
+            { name: 'Costing Records', path: 'ui/costing_records'}
+          ],
+          'D' => [
+            { name: 'Disability Records', path: 'ui/disability_records'}
+          ],
+          'E' => [
+            { name: 'Element Records', path: 'ui/element_records'}
+          ],
+          'L' => [
+            { name: 'Location Records', path: 'ui/location_records'}
+          ],
+          'O' => [
+            { name: 'Organisation Records', path: 'ui/organisation_records'}
+          ],
+          'P' => [
+            { name: 'Person Eit Records', path: 'ui/person_eit_records'},
+            { name: 'Person Records', path: 'ui/person_records'},
+            { name: 'Position Eit Records', path: 'ui/position_eit_records'},
+            { name: 'Position Records', path: 'ui/position_records'}
+          ],
+          'Q' => [
+            { name: 'Qualification Records', path: 'ui/qualification_records'}
+          ],
+          'S' => [
+            { name: 'Sit Records', path: 'ui/sit_records'}
+          ],
+          'T' => [
+            { name: 'Training Absence Records', path: 'ui/training_absence_records'}
+          ]
         }
       )
     end
