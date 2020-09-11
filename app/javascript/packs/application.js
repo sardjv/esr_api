8dfb32e37376c67d493fb214cc6f8ddc862a0837
Alex <alexrudall@users.noreply.github.com>
2020-09-11 09:38:06 +0100

    Add Turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@
 // layout file, like app/views/layouts/application.html.erb
 
 require('@rails/ujs').start()
+require('turbolinks').start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
