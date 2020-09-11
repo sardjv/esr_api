d882143c45e57a3d27ede6478c6574c5c3dbc5ba
Alex <alexrudall@users.noreply.github.com>
2020-09-11 12:18:25 +0100

    Get token_spec passing

--- spec/factories/token_factory.rb
+++ spec/factories/token_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :token do
-    name { 'Token for Javascript SPA' }
+    name { Faker::Company.unique.industry }
     token { '123token' }
     created_by_id { User.all.sample.try(:id) || create(:user).id }
   end
