be71b7f5314f0502576c405131a1ff2a4461d1a0
Alex <alexrudall@users.noreply.github.com>
2020-09-15 15:41:02 +0100

    Add tests that API endpoints need at least 1 active user

--- spec/api/v1/person_records/index_spec.rb
+++ spec/api/v1/person_records/index_spec.rb
@@ -33,22 +33,34 @@ describe 'Api::V1::PersonRecordResource', type: :request, swagger_doc: 'v1/swagg
         response '200', 'successful' do
           schema '$ref' => '#/definitions/person_records_response'
 
-          describe 'attributes match database values' do
-            run_test! do
-              expect(response_data.count).to eq(2)
-              database_record = PersonRecord.find(response_data.first['id'])
-              response_data.first['attributes'].each do |key, value|
-                expect(database_record.send(key).to_s).to eq(value.to_s)
-              end
+          context 'when there are no confirmed users' do
+            response '403', 'Error: Forbidden' do
+              schema '$ref' => '#/definitions/error_403'
+
+              run_test!
             end
           end
 
-          context 'with 1 result per page' do
-            let!(:'page[size]') { 1 }
-            let!(:'page[number]') { 2 }
+          context 'with a confirmed user' do
+            let!(:confirmed_user) { create(:confirmed_user) }
 
-            run_test! do
-              expect(response_data.length).to eq(1)
+            describe 'attributes match database values' do
+              run_test! do
+                expect(response_data.count).to eq(2)
+                database_record = PersonRecord.find(response_data.first['id'])
+                response_data.first['attributes'].each do |key, value|
+                  expect(database_record.send(key).to_s).to eq(value.to_s)
+                end
+              end
+            end
+
+            context 'with 1 result per page' do
+              let!(:'page[size]') { 1 }
+              let!(:'page[number]') { 2 }
+
+              run_test! do
+                expect(response_data.length).to eq(1)
+              end
             end
           end
         end
