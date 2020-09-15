4fac756b3cc61daf47a94415c1adb3acc2b7773b
Alex <alexrudall@users.noreply.github.com>
2020-09-15 10:28:33 +0100

    Add blind index for tokens to allow searching by unencrypted value

--- app/models/token.rb
+++ app/models/token.rb
@@ -5,6 +5,7 @@ class Token < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :created_by_id }
   validates :token, presence: true
   encrypts :token
+  blind_index :token
 
   # Tokens should only be viewed once.
   def token_one_time
@@ -16,8 +17,8 @@ class Token < ApplicationRecord
     end
   end
 
-  def self.verify(token)
-    raise VerificationError unless Token.any? { |t| t.token == token }
+  def self.verify(decrypted_token)
+    raise VerificationError unless Token.find_by(token: decrypted_token)
 
     true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_112400) do
+ActiveRecord::Schema.define(version: 2020_09_15_101900) do
 
   create_table "absence_records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "Person ID"
@@ -520,8 +520,10 @@ ActiveRecord::Schema.define(version: 2020_09_09_112400) do
     t.bigint "created_by_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token_bidx"
     t.index ["created_by_id"], name: "index_tokens_on_created_by_id"
     t.index ["name", "created_by_id"], name: "index_tokens_on_name_and_created_by_id", unique: true
+    t.index ["token_bidx"], name: "index_tokens_on_token_bidx", unique: true
     t.index ["token_ciphertext"], name: "index_tokens_on_token_ciphertext", unique: true
   end
 
