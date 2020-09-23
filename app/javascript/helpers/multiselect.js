0ef94ce7fcf8e02cae779419f1f3b6375a103964
Alex <alexrudall@users.noreply.github.com>
2020-09-23 15:06:00 +0100

    Change columns options based on resource selected

--- app/javascript/helpers/multiselect.js
+++ app/javascript/helpers/multiselect.js
@@ -1,8 +1,5 @@
 function applySelect2() {
   $('select').select2()
-  $('.multiselect').select2({
-    width: '100%'
-  })
 }
 
 window.addEventListener('turbolinks:load', () => {
@@ -12,4 +9,27 @@ window.addEventListener('turbolinks:load', () => {
 // For nested forms.
 $(document).on('cocoon:after-insert', (e, insertedItem, originalEvent) => {
   applySelect2()
+
+  $('.multiselect').select2({
+    width: '100%'
+  })
+
+  // Update permission#columns when permission#resource changed.
+  $('.resource-select').on('change', (e) => {
+    // Temporary ID used to distinguish between  multiple nested forms.
+    const id = e.target.id.split('_')[3]
+    const url = '/ui/tokens/new.json?resource=' + e.target.value
+
+    // Fetch data.
+    $.ajax({
+      url: url,
+    }).done(function(response) {
+      // Find columns selector with the same ID, and update the column options.
+      $('.multiselect').empty().select2({
+        width: '100%',
+        data: response.column_options
+      })
+    });
+
+  });
 });
