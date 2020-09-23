afea33f661b9d67b0852038fbac055eca5970ac0
Alex <alexrudall@users.noreply.github.com>
2020-09-23 11:08:36 +0100

    Make multiselect fill parent

--- app/javascript/helpers/multiselect.js
+++ app/javascript/helpers/multiselect.js
@@ -2,5 +2,7 @@ $('.js-states').select2()
 
 // For nested forms.
 $(document).on('cocoon:after-insert', (e, insertedItem, originalEvent) => {
-  $('.js-states').select2()
+  $('.js-states').select2({
+    width: '100%'
+  })
 });
