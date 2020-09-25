c70f5e4c4c9a1a294b1bc5bf4cdc4c9c917c0c82
Alex <alexrudall@users.noreply.github.com>
2020-09-25 12:52:54 +0100

    Update column selects on token form if there's an error

--- app/javascript/helpers/multiselect.js
+++ app/javascript/helpers/multiselect.js
@@ -6,21 +6,11 @@ $(document).on('cocoon:after-insert', (e, insertedItem, originalEvent) => {
 
   // Update permission#columns when permission#resource changed.
   $('.resource-select').on('change', (e) => {
-    // Temporary ID used to distinguish between  multiple nested forms.
+    // Temporary ID used to distinguish between multiple nested forms.
     const id = e.target.id.split('_')[3]
+    const target = $('#token_permissions_attributes_' + id + '_columns')
     const url = '/ui/tokens/new.json?resource=' + e.target.value
-
-    // Fetch data.
-    $.ajax({
-      url: url,
-    }).done(function(response) {
-      // Find columns selector with the same ID, and update the column options.
-      $('#token_permissions_attributes_' + id + '_columns').empty().select2({
-        width: '100%',
-        data: response.column_options
-      })
-    });
-
+    updateSelect(target, url)
   });
 });
 
@@ -29,4 +19,26 @@ window.addEventListener('turbolinks:load', () => {
   $('.multiselect').select2({
     width: '100%'
   })
+
+  // Update permission#columns when permission#resource changed.
+  $('.resource-select').on('change', (e) => {
+    // Temporary ID used to distinguish between multiple nested forms.
+    const id = e.target.id.split('_')[3]
+    const target = $('#token_permissions_attributes_' + id + '_columns')
+    const url = '/ui/tokens/new.json?resource=' + e.target.value
+    updateSelect(target, url)
+  });
 });
+
+function updateSelect(target, url) {
+  // Fetch data.
+  $.ajax({
+    url: url,
+  }).done(function(response) {
+    // Find columns selector with the same ID, and update the column options.
+    target.empty().select2({
+      width: '100%',
+      data: response.column_options
+    })
+  });
+}
