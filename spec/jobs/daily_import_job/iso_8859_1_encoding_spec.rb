a4ba1d0f2d71137632afa12e9e2ea2347ee03fa5
Alex <alexrudall@users.noreply.github.com>
2020-10-07 16:09:55 +0100

    Move non-utf-8 encoding check to a separate test

---
+++ spec/jobs/daily_import_job/iso_8859_1_encoding_spec.rb
@@ -0,0 +1,16 @@
+describe DailyImportJob, type: :job do
+  let(:add_filename) { file_fixture('iso_8859_1_encoding.dsv').to_path }
+  let(:add_job) { DailyImportJob.perform_later(filename: add_filename) }
+
+  it 'creates a new LocationRecord' do
+    perform_enqueued_jobs { add_job }
+
+    expect(LocationRecord.count).to eq(1)
+    pr = LocationRecord.first
+
+    # Expect values in the database to match input from iso_8859_1_encoding.dsv.
+    Expectations::Iso88591Encoding.added.each do |key, value|
+      expect(pr.send(key)).to eq(value)
+    end
+  end
+end
