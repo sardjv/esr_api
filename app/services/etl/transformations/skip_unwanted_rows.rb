0ff2d1e61551750c4125d3a968d746347270f88a
Alex <alexrudall@users.noreply.github.com>
2020-10-08 13:01:13 +0100

    Ignore trailer row

--- app/services/etl/transformations/skip_unwanted_rows.rb
+++ app/services/etl/transformations/skip_unwanted_rows.rb
@@ -9,6 +9,7 @@ class ETL::Transformations::SkipUnwantedRows
     [
       'HDR', # File header row.
       'CDA' # Undocumented record type? Excluding for now as not clear what the headers should be.
+      'TRL' # File trailer row.
     ].freeze
   end
 end
