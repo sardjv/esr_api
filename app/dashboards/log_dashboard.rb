80976335b213b85ec41a385b322f4b22efb0dc89
Alex <alexrudall@users.noreply.github.com>
2020-10-13 12:09:03 +0100

    Improve ordering

--- app/dashboards/log_dashboard.rb
+++ app/dashboards/log_dashboard.rb
@@ -24,10 +24,10 @@ class LogDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :trackable,
     :owner,
+    :created_at,
     :key,
-    :created_at
+    :trackable,
   ].freeze
 
   # COLLECTION_FILTERS
