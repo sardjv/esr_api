881074e5582d07ef01ce47cfe17cd8edd46ab060
Alex <alexrudall@users.noreply.github.com>
2020-10-13 14:16:06 +0100

    Get specs running

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,11 +3,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 const chokidar = require('chokidar')
 
-environment.config.devServer.before = (app, server) => {
-  chokidar.watch([
-    'config/locales/*.yml',
-    'app/views/**/*.erb'
-  ]).on('change', () => server.sockWrite(server.sockets, 'content-changed'))
+if(environment.config.devServer) {
+  environment.config.devServer.before = (_app, server) => {
+    chokidar.watch([
+      'config/locales/*.yml',
+      'app/views/**/*.erb'
+    ]).on('change', () => server.sockWrite(server.sockets, 'content-changed'))
+  }
 }
 
 module.exports = environment.toWebpackConfig()
