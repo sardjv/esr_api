8bd9f245b70a9a973e41db651a12ce31471a0214
Alex <alexrudall@users.noreply.github.com>
2020-10-15 13:10:45 +0100

    Remove redundant spec

--- spec/tasks/import_files_spec.rb
+++ spec/tasks/import_files_spec.rb
@@ -3,15 +3,6 @@ require 'rake'
 describe 'rake import_files' do
   before { EsrApi::Application.load_tasks }
 
-  context 'with filenames with timestamps' do
-    let(:imports_path) { 'spec/fixtures/files/good_imports' }
-
-    it 'creates jobs to import the files' do
-      expect(ImportFileJob).to receive(:perform_later).exactly(Dir.children(imports_path).count).times
-      Rake::Task['import_files'].execute(imports_path: imports_path)
-    end
-  end
-
   context 'with disordered filenames' do
     let(:imports_path) { 'spec/fixtures/files/disordered_imports' }
     let(:first) { Rails.root.join(imports_path, '4_20190101_00000001.DAT').to_s }
