5ca117240ae6ab228a41dd6042669f421b3428f6
Alex <alexrudall@users.noreply.github.com>
2020-10-15 13:29:18 +0100

    Fix bad filenames

--- spec/tasks/import_files_spec.rb
+++ spec/tasks/import_files_spec.rb
@@ -5,10 +5,10 @@ describe 'rake import_files' do
 
   context 'with disordered filenames' do
     let(:imports_path) { 'spec/fixtures/files/disordered_imports' }
-    let(:first) { Rails.root.join(imports_path, '4_20190101_00000001_20201015_00001157.DAT').to_s }
-    let(:second) { Rails.root.join(imports_path, '2_20200602_00001631_20201015_00001157.DAT').to_s }
-    let(:third) { Rails.root.join(imports_path, '1_20200602_00001632_20201015_00001157.DAT').to_s }
-    let(:fourth) { Rails.root.join(imports_path, '3_20221223_00002032_20201015_00001157.DAT').to_s }
+    let(:first) { Rails.root.join(imports_path, '4_20190101_00000001.DAT').to_s }
+    let(:second) { Rails.root.join(imports_path, '2_20200602_00001631.DAT').to_s }
+    let(:third) { Rails.root.join(imports_path, '1_20200602_00001632.DAT').to_s }
+    let(:fourth) { Rails.root.join(imports_path, '3_20221223_00002032.DAT').to_s }
 
     it 'creates jobs in date and time order' do
       expect(ImportFileJob).to receive(:perform_later).with(filename: first).ordered
