6a6b752df97cbe001bd69baa65957cb14d02f699
Alex <alexrudall@users.noreply.github.com>
2020-10-16 16:32:10 +0100

    Add specs for CSV export

--- spec/requests/ui/absence_records_controller_spec.rb
+++ spec/requests/ui/absence_records_controller_spec.rb
@@ -1,4 +1,6 @@
 describe Ui::AbsenceRecordsController, type: :request do
+  let!(:absence_record) { create(:absence_record) }
+
   context 'when not authenticated' do
     describe 'GET index' do
       before { get ui_absence_records_path }
@@ -6,9 +8,15 @@ describe Ui::AbsenceRecordsController, type: :request do
     end
 
     describe 'GET show' do
-      let(:absence_record) { create(:absence_record) }
       before { get ui_absence_record_path(absence_record.id) }
       it { expect(response).to redirect_to(pages_home_path) }
+    end
+
+    describe 'GET CSV export' do
+      context 'with a null date' do
+        before { get export_ui_absence_records_path, format: :csv }
+        it { expect(response).not_to be_successful }
+      end
     end
   end
 
@@ -21,9 +29,15 @@ describe Ui::AbsenceRecordsController, type: :request do
     end
 
     describe 'GET show' do
-      let(:absence_record) { create(:absence_record) }
       before { get ui_absence_record_path(absence_record.id) }
       it { expect(response).to be_successful }
+    end
+
+    describe 'GET CSV export' do
+      context 'with a null date' do
+        before { get export_ui_absence_records_path, format: :csv }
+        it { expect(response).to be_successful }
+      end
     end
   end
 end
