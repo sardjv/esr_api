a63cc7c47820af44dfa6ca431e33cbfc87152f2e
Alex <alexrudall@users.noreply.github.com>
2020-11-19 13:34:12 +0000

    Add sidekiq bundle install back. Prevents having to restart on first launch

--- script/start_sidekiq.sh
+++ script/start_sidekiq.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# `bundle install` here rather than in Dockerfile,
+# so that we can cache its result in the bundle_cache container.
+bundle install
+
 # Start Sidekiq.
 bundle exec sidekiq -C config/sidekiq.yml.erb
