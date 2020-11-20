d82a4a311e4a4e64c32c0f326348f0fef589174e
Alex <alexrudall@users.noreply.github.com>
2020-11-20 15:05:59 +0000

    Revert database to config var

--- script/start_webpacker.sh
+++ script/start_webpacker.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# `bundle install` here rather than in Dockerfile,
-# so that we can cache its result in the bundle_cache container.
-bundle install
-
-yarn install
+FIRST_RUN_FLAG_FILE=script/first_run_complete.tmp
+if [ -f $FIRST_RUN_FLAG_FILE ]
+then echo "$FIRST_RUN_FLAG_FILE detected, skipping webpacker library installation..."
+else echo "Booting for the first time, installing webpacker libraries and creating database..."
+  # Install here as well as in start_rails_development to prevent webpacker failing on initial boot.
+  bundle install
+  yarn install
+fi
 
 # Start Webpacker
 ./bin/webpack-dev-server
