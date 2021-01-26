579e1284d571ef4ea9c0e7f12f88e8b597f8a67c
Alex <7175262+gritops@users.noreply.github.com>
2021-01-26 18:49:01 +0000

    Improve script

--- script/generate_env_file.sh
+++ script/generate_env_file.sh
@@ -3,14 +3,17 @@
 # This script will copy .env.example into a new file .env, and replace
 # each instance of the string YOU_MUST_CHANGE_THIS_PASSWORD with a random
 # 64-character hex. For use on initial system setup only.
-# Requires openssl.
+# Requires bash and openssl.
+# To run:
+# sh ./script/generate_env_file.sh
+EXAMPLE_FILE=.env.example
 ENV_FILE=.env
+REPLACE=YOU_MUST_CHANGE_THIS_PASSWORD
 if [ -f $ENV_FILE ]
 then echo "$ENV_FILE detected, aborting."
 else echo "Generating a fresh $ENV_FILE file..."
-  cp .env.example $ENV_FILE
+  cp $EXAMPLE_FILE $ENV_FILE
 
-  REPLACE=YOU_MUST_CHANGE_THIS_PASSWORD
   for i in `grep $REPLACE $ENV_FILE`; do
     sed -i -e "1s/$REPLACE/$(openssl rand -hex 32)/;t" -e "1,/$REPLACE/s//$(openssl rand -hex 32)/" $ENV_FILE
   done
