abb290042cf53740762c70173e8cfa5b2a710c79
Alex <7175262+gritops@users.noreply.github.com>
2021-02-25 16:53:15 +0000

    Add module naming consistency checker

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module EsrApi
+module EsrApi # rubocop:disable Style/ClassAndModuleChildren
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
