68f4e7f0ce81245cea0035d8d6377c656c0501a1
Alex <7175262+gritops@users.noreply.github.com>
2021-03-09 17:25:27 +0000

    Get specs passing

--- app/models/source.rb
+++ app/models/source.rb
@@ -29,6 +29,7 @@ class Source < ApplicationRecord
   end
 
   def validate_name_is_different_to_source
+    return if name.nil? || source.nil?
     return unless name.downcase.gsub(/\s/, '') == source.downcase.gsub(/\s/, '')
 
     errors.add(:source, I18n.t('models.source.errors.source_must_be_secret'))
