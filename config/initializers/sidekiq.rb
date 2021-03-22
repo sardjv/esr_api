e3099971c9c4fbe048b604501f1316a5e438f177
Alex <7175262+gritops@users.noreply.github.com>
2021-03-22 17:28:54 +0000

    Disable Sidekiq Cron based on ENV var

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,10 +8,10 @@ Sidekiq.configure_client do |config|
   config.redis = sidekiq_config
 end
 
-if Rails.env.development? || Rails.env.production?
+if ENV['CRONJOBS_ACTIVE'] == 'true'
   # Cronjob to import data every day.
   # Unique on name parameter; recreating will overwrite any existing job with the same name.
-  Sidekiq::Cron::Job.create(
+    Sidekiq::Cron::Job.create(
     name: 'ImportFromFtpJob',
     cron: '0 5 * * *', # Every day at 05:00 AM.
     class: 'ImportFromFtpJob',
