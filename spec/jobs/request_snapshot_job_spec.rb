19e886f1a26b5cf6da18bfed2b20033b26c71131
Alex <7175262+gritops@users.noreply.github.com>
2021-04-01 10:32:34 +0000

    Add test for system active check

--- spec/jobs/request_snapshot_job_spec.rb
+++ spec/jobs/request_snapshot_job_spec.rb
@@ -1,11 +1,21 @@
 describe RequestSnapshotJob, type: :job do
-  let!(:admin) { create(:confirmed_user) }
-  let!(:ftp_credential) { create(:ftp_credential) }
-
   describe 'perform' do
-    it 'requests the snapshot' do
-      expect_any_instance_of(FtpCredential).to receive(:request_snapshot).once
-      RequestSnapshotJob.perform_now
+    context 'with no admins' do
+      it 'does not run the job' do
+        ActiveJob.quiet do
+          expect { RequestSnapshotJob.perform_now }.to raise_error(NoActiveUsersError)
+        end
+      end
+    end
+
+    context 'with an admin' do
+      let!(:ftp_credential) { create(:ftp_credential) }
+      let!(:admin) { create(:confirmed_user) }
+
+      it 'requests the snapshot' do
+        expect_any_instance_of(FtpCredential).to receive(:request_snapshot).once
+        RequestSnapshotJob.perform_now
+      end
     end
   end
 end
