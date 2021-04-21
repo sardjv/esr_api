f18c1efdb8568becdac1b57c74e22616b037f7e2
Alex <7175262+gritops@users.noreply.github.com>
2021-04-21 10:49:09 +0000

    Update docs description

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -57,7 +57,7 @@ function Home() {
   return (
     <Layout
       title={`Documentation`}
-      description="Description will go into a meta tag in <head />">
+      description="Documentation for the ESR wrapper project.">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
