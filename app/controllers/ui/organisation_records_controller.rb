29c887c8f177d8e97e724fa3dd9c1102915befaa
Kevin Monk <kevin@sardjv.co.uk>
2022-06-23 11:55:03 +0100

    Organisation hierarchy

--- app/controllers/ui/organisation_records_controller.rb
+++ app/controllers/ui/organisation_records_controller.rb
@@ -1,4 +1,8 @@
 class Ui::OrganisationRecordsController < Ui::ApplicationController
   # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
   # for more information
+  def index
+    @root_organisation_record = OrganisationRecord.where('Parent Organisation ID': nil).first
+    super
+  end
 end
--- app/models/organisation_record.rb
+++ app/models/organisation_record.rb
@@ -22,4 +22,6 @@
 #
 class OrganisationRecord < ApplicationRecord
     belongs_to :location, class_name: 'LocationRecord', foreign_key: 'Location ID', primary_key: 'Location ID'
+    belongs_to :parent, class_name: 'OrganisationRecord', foreign_key: 'Parent Organisation ID', primary_key: 'Organisation ID'
+    has_many :children, class_name: 'OrganisationRecord', foreign_key: 'Parent Organisation ID', primary_key: 'Organisation ID'
 end
